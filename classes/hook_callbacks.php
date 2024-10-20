ee0b5fe4845f2975d9cc5e82e19914965340be3d
kudakannon <stephenhorsburgh@hotmail.com>
2024-10-20 20:12:05 +1000

    Adding the changes to the chat bot

--- classes/hook_callbacks.php
+++ classes/hook_callbacks.php
@@ -26,27 +26,37 @@ class hook_callbacks
   public static function before_standard_head_html_generation(before_standard_head_html_generation $hook): void
   {
     global $PAGE;
-
+    
     // this is not secure to expose the API key to client side, but let's do it for now
     $PAGE->requires->js_call_amd('local_teacheraide/app-lazy', 'init', [[
-      // "endpoint" => get_config('local_teacheraide', 'api_endpoint'),
-      // "apiVersion" => get_config('local_teacheraide', 'api_version'),
-      // "deployment" => get_config('local_teacheraide', 'api_deployment'),
-      // "apiKey" => get_config('local_teacheraide', 'api_key'),
-      // "model" => get_config('local_teacheraide', 'api_model'),
-      // "baseURL" => get_config('local_teacheraide', 'api_base_url'),
-      "systemPrompt" => get_config('local_teacheraide', 'system_prompt'),
+    //   // "endpoint" => get_config('local_teacheraide', 'api_endpoint'),
+    //   // "apiVersion" => get_config('local_teacheraide', 'api_version'),
+    //   // "deployment" => get_config('local_teacheraide', 'api_deployment'),
+    //   // "apiKey" => get_config('local_teacheraide', 'api_key'),
+    //   // "model" => get_config('local_teacheraide', 'api_model'),
+    //   // "baseURL" => get_config('local_teacheraide', 'api_base_url'),
+    //   "systemPrompt" => get_config('local_teacheraide', 'system_prompt'),
     ]]);
   }
   public static function before_footer_html_generation(before_footer_html_generation $hook): void {}
 
   public static function after_standard_main_region_html_generation(after_standard_main_region_html_generation $hook): void
   {
+    global $OUTPUT;
     // // test widget
+    echo $OUTPUT->box_start();
+    echo $OUTPUT->render_from_template('local_teacheraide/chat_drawer', [
+      'msg' => "Hello World Full Page",
+    ]);
+    echo $OUTPUT->box_end();
     // $hook->add_html($hook->renderer->render_from_template('local_teacheraide/test', [
     //   'msg' => "Hello World",
     // ]));
   }
 
-  public static function before_standard_top_of_body_html_generation(before_standard_top_of_body_html_generation $hook): void {}
+  public static function before_standard_top_of_body_html_generation(before_standard_top_of_body_html_generation $hook): void {
+    global $PAGE;
+    global $OUTPUT;
+    $PAGE->set_headingmenu($OUTPUT->render_from_template('local_teacheraide/chat_popover', []));
+  }
 }
