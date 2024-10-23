ccfb8191f3554b22681ca11ee951ab80cfbba500
kudakannon <stephenhorsburgh@hotmail.com>
2024-10-24 07:12:07 +1000

    Update one the UI

--- vue/src/store/chatbox.ts
+++ vue/src/store/chatbox.ts
@@ -41,8 +41,5 @@ export const useChatboxStore = defineStore("chatbox", {
         ...state.userMessages,
       ] satisfies ChatMessage[];
     },
-  },
-  persist: {
-    storage: localStorage,
-  },
+  }
 });
