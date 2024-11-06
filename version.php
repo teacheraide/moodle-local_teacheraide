4ea1b99286b61d31a405427767d912802823786b
Freeman Man <freeman@ycfreeman.com>
2024-11-06 23:23:18 +1000

    bump plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_teacheraide';
-$plugin->release = '0.1.0';
-$plugin->version = 2024091002;
+$plugin->release = '0.2.0';
+$plugin->version = 2024110601;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
